a74439061a8112596449bb07138506ff5535d83e
Cynthia "Arty" Ng <cng@gitlab.com>
2025-01-03 16:09:14 +0000

    Fix push command

--- scripts/update-counts.sh
+++ scripts/update-counts.sh
@@ -31,13 +31,13 @@ push_to_main () {
     cd /tmp/handbook
     git config --global user.email "$GITLAB_USER_EMAIL"
     git config --global user.name "$GITLAB_USER_NAME"
+    git remote add origin https://bot:$GITLAB_TOKEN@gitlab.com/gitlab-com/content-sites/handbook.git
     branch_name="update-counts-${TODAY}"
     git checkout -b "$branch_name"
     git add assets/csv/about-count.csv
     git add assets/csv/handbook-count.csv
     git commit -m "Update handbook word and page counts"
-    echo $GITLAB_USER_LOGIN
-    git push origin "$branch_name" -o merge_request.create -o merge_request.title="$TODAY Update handbook word and page counts" -o merge_request.description="Performs the quarterly handbook word and page counts" -o merge_request.label="Handbook::Operations" -o merge_request.label="type::maintenance"
+    git push origin "$branch_name" -o merge_request.create -o merge_request.title="$TODAY Update handbook word and page counts" -o merge_request.description="Performs the quarterly handbook word and page counts" -o merge_request.label="Handbook::Operations" -o merge_request.label="type::maintenance" -o merge_request.assign="$GITLAB_USER_LOGIN"
 }
 
 if [ "$RUN_TYPE" = "count-update" ]; then
