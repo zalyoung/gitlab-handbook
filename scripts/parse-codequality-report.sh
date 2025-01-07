0c25b9ddee1945c64aca3b4e65d24dc7bd8c8321
Cynthia 'Arty' Ng <cng@gitlab.com>
2025-01-06 16:31:50 -0800

    Fix comment parser

--- scripts/parse-codequality-report.sh
+++ scripts/parse-codequality-report.sh
@@ -77,19 +77,16 @@ generate_table() {
     done
 
     # Process linkcheck report
-    echo $LREPORT
     LENGTH=$(jq '. | length' $LREPORT)
-    echo LENGTH
     for i in $(seq 0 $((LENGTH-1))); do
-      echo "Processing linkcheck report i"
+      ERROR=$(jq -r ".[$i].check_name" $LREPORT | cut -d '/' -f 1)
       FILE=$(jq -r ".[$i].location.path" $LREPORT)
       LINE=$(jq -r ".[$i].location.lines.begin" $LREPORT)
       LOC="$REPO_URL/-/blob/$CI_COMMIT_SHA/$FILE#L$LINE"
       DESCRIPTION=$(jq -r ".[$i].description" $LREPORT | cut -d ':' -f 2-)
       ERRORS+=( $ERROR )
       if [[ "$ERROR" ]]; then
-        echo "Found $ERROR"
-        MSG+="| Broken link | [$FILE]($LOC) | [$LINE]($LOC) | $DESCRIPTION |\n"
+        MSG+="| Broken link ($ERROR) | [$FILE]($LOC) | [$LINE]($LOC) | $DESCRIPTION |\n"
       fi
     done
 
