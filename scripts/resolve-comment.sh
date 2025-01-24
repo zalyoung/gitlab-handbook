503affbfbc262df88c1bd3826e9e20864a0dac39
Raimund Hook <rhook@gitlab.com>
2025-01-24 18:52:10 +0000

    Updating to simply overwrite the initial message
    Signed-off-by: Raimund Hook <rhook@gitlab.com>

--- scripts/resolve-comment.sh
+++ scripts/resolve-comment.sh
@@ -9,7 +9,7 @@
 glab auth login -t $GITLAB_TOKEN
 user_id=$(glab api user | jq .id)
 
-resolution_msg="Previous failures have been resolved. Resolving this thread!"
+resolution_msg="Previous linting failures have been resolved. Resolving this thread!"
 
 # Search for any existing notes by our bot user.
 note_id=$(glab api projects/$CI_PROJECT_ID/merge_requests/$CI_MERGE_REQUEST_IID/notes | jq -c "last(.[] | select( .author | .id | contains($user_id))) | .id")
@@ -17,10 +17,7 @@ note_id=$(glab api projects/$CI_PROJECT_ID/merge_requests/$CI_MERGE_REQUEST_IID/
 # If the note already exists, find the discussion and resolve it
 if [ $note_id != null ]; then
   echo "Found existing note with id: $note_id"
-  discussion_id=$(glab api projects/$CI_PROJECT_ID/merge_requests/$CI_MERGE_REQUEST_IID/discussions | jq -r "last(.[] | select(.notes[].author.id == $user_id) | .id)")
-  echo "Found discussion with id: $discussion_id"
-  glab api projects/$CI_PROJECT_ID/merge_requests/$CI_MERGE_REQUEST_IID/discussions/$discussion_id/notes -X POST -f body="$resolution_msg"
-  glab api projects/$CI_PROJECT_ID/merge_requests/$CI_MERGE_REQUEST_IID/discussions/$discussion_id -X PUT -f resolved=true
+  glab api projects/$CI_PROJECT_ID/merge_requests/$CI_MERGE_REQUEST_IID/notes/$note_id -X PUT -f body="$resolution_msg"
 else
   echo "No previous note found, nothing to do here :)"
 fi
