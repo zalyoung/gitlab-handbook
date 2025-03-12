d91d502befd682c1f2a180d1d4813e3d8c891b12
Raimund Hook <rhook@gitlab.com>
2025-03-12 11:05:10 +0000

    Add Workaround for scripts so we can extract some more data
    Signed-off-by: Raimund Hook <rhook@gitlab.com>

--- scripts/post-comment.sh
+++ scripts/post-comment.sh
@@ -11,9 +11,17 @@ MSG=$1
 # Log In to glab CLI https://gitlab.com/gitlab-org/cli
 glab auth login -t $GITLAB_TOKEN
 user_id=$(glab api user | jq .id)
+echo "User ID: $user_id"
 
 # Search for any existing notes by our bot user.
 note_id=$(glab api projects/$CI_PROJECT_ID/merge_requests/$CI_MERGE_REQUEST_IID/notes | jq -c "last(.[] | select( .author | .id | contains($user_id))) | .id")
+
+# note: temp workaround for https://gitlab.com/gitlab-com/content-sites/handbook/-/issues/461
+if [[ $GITLAB_USER_LOGIN =~ /^(project|group)_/ ]]; then
+  echo "Skipping for project or group bot token authored MRs."
+  echo "See https://gitlab.com/gitlab-com/content-sites/handbook/-/issues/461"
+  exit 0
+fi
 
 # If the note already exists, update the message to reduce MR notes.
 if [ $note_id != null ]; then
